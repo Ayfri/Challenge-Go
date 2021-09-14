89d644f485b26b98a09d83601b40501e8539597b
Ayfri <pierre.ayfri@gmail.com>
2021-09-14 16:42:01 +0200

    fix: Fix factorial.

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,16 +2,12 @@ package piscine
 
 func IterativeFactorial(nb int) int {
 	result := 1
-	if nb > 1000000000 || nb < 0 {
+	if nb > 1_000_000_000 || nb < 0 {
 		return 0
 	}
 
-	if nb == 1 {
-		return 1
-	}
-
 	for i := nb; i > 0; i-- {
-		result += i * nb
+		result *= i
 	}
 
 	return result
