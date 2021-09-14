a03fffa13d6dcc3428278ffa52df0328d4e8fd57
Ayfri <pierre.ayfri@gmail.com>
2021-09-14 16:29:35 +0200

    fix: Fix returning value of max number.

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,8 +2,8 @@ package piscine
 
 func IterativeFactorial(nb int) int {
 	result := 1
-	if nb > 1000000002 || nb < 1 {
-		return result
+	if nb > 1000000001 {
+		return 0
 	}
 
 	for i := nb; i > 0; i-- {
