51772b7df022ae2de4197c9d81042470b8e6bf7a
Ayfri <pierre.ayfri@gmail.com>
2021-09-14 11:05:24 +0200

    feat: Add fifth challenge.

---
+++ printcomb.go
@@ -0,0 +1,23 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintComb() {
+	const newLine = rune('\n')
+	const zero = rune('0')
+	const nine = rune('9')
+
+	for i := zero; i <= nine; i++ {
+		for j := zero; j <= nine; j++ {
+			for k := zero; k <= nine; k++ {
+				if i == j || j == k || i == k {
+					continue
+				}
+
+				z01.PrintRune(i)
+				z01.PrintRune(j)
+				z01.PrintRune(k)
+			}
+		}
+	}
+}
