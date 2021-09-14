22b09769f52468a34a9bac2308c0f75b1de8b287
Ayfri <pierre.ayfri@gmail.com>
2021-09-14 14:15:16 +0200

    fix: Fix using big negative numbers.

--- printnbr.go
+++ printnbr.go
@@ -7,18 +7,20 @@ import (
 func PrintNbr(nbr int) {
 	str := ""
 	isNegative := nbr < 0
-	if isNegative {
-		nbr = -nbr
-	}
 
 	for i, remainder := nbr, 0; i != 0; i /= 10 {
 		remainder = i % 10
+		if isNegative {
+			remainder = -remainder
+		}
 		char := rune(remainder) + rune('0')
 		str += string(char)
 	}
+
 	if isNegative {
 		str += "-"
 	}
+
 	str = Reverse(str)
 
 	for _, char := range str {
