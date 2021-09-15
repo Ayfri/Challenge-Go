a419b2e3420ffa2bf242f6cd828891fd655e7a0c
Ayfri <pierre.ayfri@gmail.com>
2021-09-15 16:59:43 +0200

    fix: Skip if char is not a letter.

--- isupper.go
+++ isupper.go
@@ -2,9 +2,16 @@ package piscine
 
 func IsUpper(s string) bool {
 	for _, char := range s {
-		if char >= 'a' && char <= 'z' {
+		if !isLetter(char) {
+			continue
+		}
+		if char >= 'A' && char <= 'Z' {
 			return false
 		}
 	}
 	return true
+}
+
+func isLetter(char rune) bool {
+	return char >= 'A' && char <= 'Z' || char >= 'a' && char <= 'z'
 }
