207534f86521caedc4cb30fd7c46cef00be5412c
Ayfri <pierre.ayfri@gmail.com>
2021-09-15 16:36:13 +0200

    refactor: Reformat code.

--- index.go
+++ index.go
@@ -3,8 +3,8 @@ package piscine
 // Index : find index
 func Index(s string, toFind string) int {
 	for index := range s {
-		if index + len(toFind) > len(s) {
-		    continue
+		if index+len(toFind) > len(s) {
+			continue
 		}
 		if s[index:len(toFind)] == toFind {
 			return index
