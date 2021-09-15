58b3b25af49107f082d2b380dabd26d6bb33f215
Ayfri <pierre.ayfri@gmail.com>
2021-09-15 08:13:55 +0200

    feat: Add quest 2 challenge 2.

---
+++ recursivefactorial.go
@@ -0,0 +1,13 @@
+package piscine
+
+func RecursiveFactorial(nb int) int {
+	if nb == 0 {
+		return 1
+	}
+
+	if nb > 1_000_000_000 || nb < 0 {
+		return 0
+	}
+
+	return nb * RecursiveFactorial(nb - 1)
+}
