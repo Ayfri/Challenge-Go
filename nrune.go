a509dc4a3725171adb0cf1fd1cbd0cde3b7db9e2
Ayfri <pierre.ayfri@gmail.com>
2021-09-15 15:16:58 +0200

    fix: Return 0 with good condition.

--- nrune.go
+++ nrune.go
@@ -1,13 +1,9 @@
 package piscine
 
 func NRune(s string, n int) rune {
-	if n+1 > len(s) {
+	if n-1 > len(s) {
 		return 0
 	}
 
-	char := rune(s[n-1])
-	if char == '\x00' {
-		return '!'
-	}
-	return char
+	return rune(s[n-1])
 }
