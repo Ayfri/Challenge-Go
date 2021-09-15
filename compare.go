d576c89b3f325a00027571efecd945ecfcaea91f
Ayfri <pierre.ayfri@gmail.com>
2021-09-15 15:28:59 +0200

    fix: Fix conditions.

--- compare.go
+++ compare.go
@@ -2,10 +2,10 @@ package piscine
 
 func Compare(a, b string) int {
 	if len(a) > len(b) {
-		return -1
-	} else if len(a) == len(b) {
-		return 0
-	} else {
 		return 1
+	} else if len(a) < len(b) {
+		return -1
+	} else {
+		return 0
 	}
 }
