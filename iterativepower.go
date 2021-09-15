0bdae383e2a92195dfa89cb42833a37eb2853513
Ayfri <pierre.ayfri@gmail.com>
2021-09-15 08:54:35 +0200

    fix: Fix returning value of negative powers.

--- iterativepower.go
+++ iterativepower.go
@@ -3,11 +3,11 @@ package piscine
 func IterativePower(nb, power int) int {
 	result := 0
 
-	if nb < 0 {
+	if nb < 0 || power <= 0 {
 		return 0
 	}
 
-	if nb == 0 || power <= 0 {
+	if nb == 0 {
 		return 1
 	}
 
