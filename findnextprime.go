d1c7c9ef0af4eec6f57b9b33eeb3545444ae210d
Ayfri <pierre.ayfri@gmail.com>
2021-09-15 11:57:06 +0200

    fix: Use a more optimized IsPrime.

--- findnextprime.go
+++ findnextprime.go
@@ -2,14 +2,23 @@ package piscine
 
 func FindNextPrime(nb int) int {
 	if nb > 1 {
-		if IsPrime(nb) {
+		if IsPrimeOptimized(nb) {
 			return nb
 		}
 		for i := nb; i < (i << 31); i++ {
-			if IsPrime(i) {
+			if IsPrimeOptimized(i) {
 				return i
 			}
 		}
 	}
 	return 2
+}
+
+func IsPrimeOptimized(nb int) bool {
+	for i := 2; i <= nb/2; i += 2 {
+		if nb%i == 0 {
+			return false
+		}
+	}
+	return true
 }
