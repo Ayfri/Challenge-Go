c62ce7020025b94564f577d83430a3ca200b07db
Ayfri <pierre.ayfri@gmail.com>
2021-09-15 10:19:19 +0200

    feat: Add quest 2 challenge 5.

---
+++ fibonacci.go
@@ -0,0 +1,15 @@
+package piscine
+
+func Fibonacci(index int) int {
+	result := 1
+
+	if index < 0 {
+		return -1
+	}
+
+	if index > 1 {
+		result = Fibonacci(index - 1) + Fibonacci(index - 2)
+	}
+
+	return result
+}
