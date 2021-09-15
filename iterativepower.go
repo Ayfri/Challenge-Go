73288dfd4d0a66c93c9e193170bd0ad644f5ca1d
Ayfri <pierre.ayfri@gmail.com>
2021-09-15 09:10:03 +0200

    fix: Fix conditions.

--- iterativepower.go
+++ iterativepower.go
@@ -1,15 +1,22 @@
 package piscine
 
 func IterativePower(nb, power int) int {
-	result := 0
+	result := 1
 
-	if nb < 0 || power <= 0 {
-		return 0
+	if nb < 0 {
+		if power == 1 {
+			return nb
+		}
+		nb = -nb
 	}
 
 	if nb == 0 {
-		return 1
+		return result
 	}
+	if power <= 0 {
+		return 0
+	}
+
 
 	for i := 0; i < nb; i++ {
 		result *= power
