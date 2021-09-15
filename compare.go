ff4217fa6e23cc16851e19fd1381577af67e8b8d
Ayfri <pierre.ayfri@gmail.com>
2021-09-15 15:30:07 +0200

    refactor: Simplify conditions.

--- compare.go
+++ compare.go
@@ -1,11 +1,11 @@
 package piscine
 
 func Compare(a, b string) int {
-	if len(a) > len(b) {
-		return 1
-	} else if len(a) < len(b) {
-		return -1
-	} else {
+	if a == b {
 		return 0
 	}
+	if a < b {
+		return -1
+	}
+	return +1
 }
