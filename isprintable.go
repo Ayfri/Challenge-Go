12580377260f7babda300ee96ed159a778dedeb8
Ayfri <pierre.ayfri@gmail.com>
2021-09-16 11:33:24 +0200

    fix: Add missing non-printable chars, use array.

--- isprintable.go
+++ isprintable.go
@@ -1,12 +1,16 @@
 package piscine
 
 func IsPrintable(s string) bool {
+	nonPrintable := []rune("\b\r\t\n\f")
+
 	if len(s) < 1 {
 		return false
 	}
 	for _, char := range s {
-		if char == '\t' || char == '\n' {
-			return false
+		for _, nonPrintableChar := range nonPrintable {
+			if char == nonPrintableChar {
+				return false
+			}
 		}
 	}
 	return true
