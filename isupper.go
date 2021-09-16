2990ddde43370c69ef9cf8cdd1185dfbd2ed4964
Ayfri <pierre.ayfri@gmail.com>
2021-09-16 08:11:16 +0200

    fix: Return false if string doesn't contains letters.

--- isupper.go
+++ isupper.go
@@ -1,13 +1,18 @@
 package piscine
 
 func IsUpper(s string) bool {
+	containsLetter := false
 	for _, char := range s {
 		if !isLetter(char) {
+			containsLetter = true
 			continue
 		}
 		if char <= 'A' || char >= 'Z' {
 			return false
 		}
+	}
+	if !containsLetter {
+		return false
 	}
 	return true
 }
