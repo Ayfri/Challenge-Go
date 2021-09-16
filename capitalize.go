1205835cd591f8d964375c35555173b9090a6232
Ayfri <pierre.ayfri@gmail.com>
2021-09-16 15:34:00 +0200

    fix: Un-capitalize capitalized letters when not-word.

--- capitalize.go
+++ capitalize.go
@@ -3,14 +3,25 @@ package piscine
 func Capitalize(s string) string {
 	result := ""
 	isWord := false
+	difference := 'A' - 'a'
 	for _, char := range s {
-		if isWord && isLetter(char) {
-			char += 'A' - 'a'
+		if isLetter(char) {
+			if isWord {
+				if !isUpper(char) {
+					char += difference
+				}
+			} else if isUpper(char) {
+				char -= difference
+			}
 		}
 		isWord = isSpace(char)
 		result += string(char)
 	}
 	return result
+}
+
+func isUpper(char rune) bool {
+	return char <= 'a' && char >= 'z'
 }
 
 func isSpace(r rune) bool {
