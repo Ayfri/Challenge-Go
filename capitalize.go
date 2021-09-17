c9378b52d425b58342ef9b94223fcf1a5fc2d247
Ayfri <pierre.ayfri@gmail.com>
2021-09-17 08:15:16 +0200

    fix: Fix conditions.

--- capitalize.go
+++ capitalize.go
@@ -1,30 +1,44 @@
 package piscine
 
-func Capitalize(s string) string {
-	result := ""
-	isWord := true
-	difference := 'A' - 'a'
-	for _, char := range s {
-		newChar := char
-		if isLetter(char) {
-			if isWord {
-				if !isUpper(char) {
-					newChar += difference
-				}
-			} else if isUpper(char) {
-				newChar -= difference
-			}
-		}
-		isWord = isSpace(char)
-		result += string(newChar)
+func isAlpha(a rune) bool {
+	return (a >= 'A' && a <= 'Z') || (a >= 'a' && a <= 'z') || (a >= '0' && a <= '9')
+}
+
+func toUpper(s rune) rune {
+	if s-32 < 65 {
+		return s
 	}
-	return result
+	return rune(s - 32)
 }
 
-func isUpper(char rune) bool {
-	return char <= 'a' && char >= 'z'
+func toLower(s rune) rune {
+	if s+32 > 122 {
+		return s
+	}
+	return rune(s + 32)
 }
 
-func isSpace(r rune) bool {
-	return !IsAlpha(string(r))
+func Capitalize(s string) string {
+	runes := []rune(s)
+	length := 0
+	ok := true
+	for i := range s {
+		length = i
+	}
+
+	for i := 0; i < length; i++ {
+		if isAlpha(runes[i]) == true && ok {
+			if runes[i] >= 'a' && runes[i] <= 'z' {
+				runes[i] = toUpper(runes[i])
+			} else if runes[i] >= '0' && runes[i] <= '9' {
+				runes[i+1] = toLower(runes[i+1])
+			}
+			ok = false
+		} else if runes[i] >= 'A' && runes[i] <= 'Z' {
+			runes[i] = toLower(runes[i])
+		} else if isAlpha(runes[i]) == false {
+			ok = true
+		}
+	}
+	return string(runes)
 }
