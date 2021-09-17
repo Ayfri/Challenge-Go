c3690e4853c5150c1e05c166bfa0e78eaf38f18b
Ayfri <pierre.ayfri@gmail.com>
2021-09-17 16:24:47 +0200

    fix: Test range.

--- split.go
+++ split.go
@@ -6,11 +6,12 @@ func Split(s, sep string) []string {
 	var isValid bool
 
 	for index, char := range s {
-		if index+len(sep) >= len(s) {
-			continue
-		}
 		isValid = true
 		for i := 0; i < len(sep); i++ {
+			if index-len(sep) < 0 || index+i >= len(s) {
+				continue
+			}
+
 			if sep != s[index-len(sep)+i:index+i] {
 				isValid = false
 			}
