617dab3511598059743df7eb4a5bfd6a1fe67e78
Ayfri <pierre.ayfri@gmail.com>
2021-09-17 14:27:52 +0200

    feat: Add quest 4 challenge 2.

---
+++ makerange.go
@@ -0,0 +1,15 @@
+package piscine
+
+func MakeRange(min, max int) []int {
+	var arr []int
+
+	if min > max {
+		return nil
+	}
+
+	for i := min; i < max; i++ {
+		arr = append(arr, i)
+	}
+
+	return arr
+}
