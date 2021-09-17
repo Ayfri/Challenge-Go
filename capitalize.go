d336cd5793faa3d833f3217060394f7a9853462d
Ayfri <pierre.ayfri@gmail.com>
2021-09-17 11:29:32 +0200

    fix: Fix capitalize.

--- capitalize.go
+++ capitalize.go
@@ -1,47 +1,30 @@
 package piscine
 
-func trust(a rune) bool {
-	if (a >= 'A' && a <= 'Z') || (a >= 'a' && a <= 'z') || (a >= '0' && a <= '9') {
-		return true
-	}
-	return false
-}
-
-func toUpper(s rune) rune {
-	if s-32 < 65 {
-		return s
-	}
-	return rune(s - 32)
-}
-
-func toLower(s rune) rune {
-	if s+32 > 122 {
-		return s
-	}
-	return rune(s + 32)
-}
-
 func Capitalize(s string) string {
-	srunes := []rune(s)
-	len := 0
-	ok := true
-	for i := range s {
-		len = i
-	}
-
-	for i := 0; i < len; i++ {
-		if trust(srunes[i]) == true && ok {
-			if srunes[i] >= 'a' && srunes[i] <= 'z' {
-				srunes[i] = toUpper(srunes[i])
-			} else if srunes[i] >= '0' && srunes[i] <= '9' {
-				srunes[i+1] = toLower(srunes[i+1])
+	result := ""
+	isWord := true
+	difference := 'A' - 'a'
+	for _, char := range s {
+		newChar := char
+		if isLetter(char) {
+			if isWord {
+				if !IsUpperChar(char) {
+					newChar += difference
+				}
+			} else if IsUpperChar(char) {
+				newChar -= difference
 			}
-			ok = false
-		} else if srunes[i] >= 'A' && srunes[i] <= 'Z' {
-			srunes[i] = toLower(srunes[i])
-		} else if trust(srunes[i]) == false {
-			ok = true
 		}
+		isWord = isSpace(char)
+		result += string(newChar)
 	}
-	return string(srunes)
+	return result
+}
+
+func IsUpperChar(char rune) bool {
+	return char >= 'A' && char <= 'Z'
+}
+
+func isSpace(r rune) bool {
+	return !IsAlpha(string(r))
 }
