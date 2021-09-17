88f5d2e4adad95c4df0408b57160e798e88627f3
Ayfri <pierre.ayfri@gmail.com>
2021-09-17 14:10:42 +0200

    fix: Fix direction of string.

--- trimatoi.go
+++ trimatoi.go
@@ -5,6 +5,7 @@ func TrimAtoi(s string) int {
 	str := ""
 	isNegative := false
 	for index, char := range s {
+		println("char[", index, "] == ", char)
 		if index > 0 && s[index-1] == '-' && len(str) == 0 {
 			isNegative = true
 		}
@@ -14,11 +15,24 @@ func TrimAtoi(s string) int {
 		}
 	}
 
+	str = Reverse(str)
+
 	for index, char := range str {
-		result += IterativePower(int(char-'0'), index-1)
+		nb := int(char - '0')
+		result += IterativePower(10, index) * nb
 	}
+
 	if isNegative {
 		result = -result
 	}
+	
 	return result
+}
+
+func Reverse(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
 }
