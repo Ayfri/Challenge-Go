c212ece90e3f052807551b41ae733142b7985425
Ayfri <pierre.ayfri@gmail.com>
2021-09-17 16:12:48 +0200

    feat: Add quest 4 challenge 6.

---
+++ split.go
@@ -0,0 +1,20 @@
+package piscine
+
+func Split(s, sep string) []string {
+	var result []string
+	var cache string
+
+	for index, char := range s {
+		if sep == s[index:index+len(sep)] {
+			cache += string(char)
+		} else if len(cache) > 0 {
+			result = append(result, cache)
+			cache = ""
+		}
+	}
+	if len(cache) > 0 {
+		result = append(result, cache)
+	}
+
+	return result
+}
