8007035c4e6553e2b34dd0f7c908130f3c4681e9
Ayfri <pierre.ayfri@gmail.com>
2021-09-17 16:23:33 +0200

    fix: Iterate through len of seperator.

--- split.go
+++ split.go
@@ -3,12 +3,20 @@ package piscine
 func Split(s, sep string) []string {
 	var result []string
 	var cache string
+	var isValid bool
 
 	for index, char := range s {
 		if index+len(sep) >= len(s) {
 			continue
 		}
-		if sep != s[index:index+len(sep)] {
+		isValid = true
+		for i := 0; i < len(sep); i++ {
+			if sep != s[index-len(sep)+i:index+i] {
+				isValid = false
+			}
+		}
+
+		if isValid {
 			cache += string(char)
 		} else if len(cache) > 0 {
 			result = append(result, cache)
