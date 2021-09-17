e60dd7979939e57e3cf480805d33e94b3d0bfb41
Ayfri <pierre.ayfri@gmail.com>
2021-09-17 11:37:18 +0200

    fix: Use separator.

--- join.go
+++ join.go
@@ -1,8 +1,12 @@
 package piscine
 
 func Join(elems []string, sep string) string {
-	for _, str := range elems {
-		sep += str
+	var result string
+	for index, str := range elems {
+		result += str
+		if index < len(elems)-1 {
+			result += sep
+		}
 	}
-	return sep
+	return result
 }
