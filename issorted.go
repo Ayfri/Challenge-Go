46108c417867f1a54ecddfd9c35637523ed5fb08
Ayfri <pierre.ayfri@gmail.com>
2021-09-19 14:34:10 +0200

    fix: Fix isSorted.

--- issorted.go
+++ issorted.go
@@ -2,9 +2,21 @@ package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
 	isSorted := true
+	if len(a) < 2 {
+		return true
+	}
+
+	isReversed := f(a[0], a[1]) > 0
 	for index, elem := range a {
-		if index+1 < len(a) && f(elem, a[index+1]) > 0 {
-			isSorted = false
+		if index+1 < len(a) {
+			elemAfter := a[index+1]
+			if f(elem, elemAfter) <= 0 {
+				if isReversed {
+					isSorted = false
+				}
+			} else if !isReversed {
+				isSorted = false
+			}
 		}
 	}
 	return isSorted
