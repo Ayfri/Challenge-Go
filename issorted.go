0bbee352356d952a7f57c62f7e41d019db0be162
Ayfri <pierre.ayfri@gmail.com>
2021-09-19 13:10:34 +0200

    feat: Add quest 5 challenge 5.

---
+++ issorted.go
@@ -0,0 +1,11 @@
+package piscine
+
+func IsSorted(f func(a, b int) int, a []int) bool {
+	isSorted := true
+	for index, elem := range a {
+		if index + 1 < len(a) && f(elem, a[index+1]) < 0 {
+			isSorted = false
+		}
+	}
+	return isSorted
+}
