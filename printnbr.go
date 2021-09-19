0df17059eaa26293e55a3d32cf8c759887c45460
Ayfri <pierre.ayfri@gmail.com>
2021-09-19 16:04:55 +0200

    fix: Remove duplicated Reverse function, rename doop.go to main.go.

--- printnbr.go
+++ printnbr.go
@@ -29,11 +29,3 @@ func PrintNbr(nbr int) {
 		z01.PrintRune(char)
 	}
 }
-
-func Reverse(s string) string {
-	runes := []rune(s)
-	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-		runes[i], runes[j] = runes[j], runes[i]
-	}
-	return string(runes)
-}
