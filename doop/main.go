a8f4ad83c2e83e0abe0cb3b777394fcf60f4c2e7
Ayfri <pierre.ayfri@gmail.com>
2021-09-19 16:13:00 +0200

    fix: Use printNbr.

--- doop/main.go
+++ doop/main.go
@@ -2,35 +2,68 @@ package main
 
 import (
 	"os"
-	"piscine"
 )
 
 func doop(value int, operator string, other int) {
 	switch operator {
 	case "+":
-		piscine.PrintNbr(value + other)
+		printInt(value + other)
 
 	case "-":
-		piscine.PrintNbr(value - other)
+		printInt(value - other)
 
 	case "*":
-		piscine.PrintNbr(value * other)
+		printInt(value * other)
 
 	case "/":
 		if value == 0 && other == value {
 			os.Stdout.WriteString("No division by 0")
 		} else {
-			piscine.PrintNbr(value / other)
+			printInt(value / other)
 		}
 
 	case "%":
 		if value == 0 && other == value {
 			os.Stdout.WriteString("No modulo by 0")
 		} else {
-			piscine.PrintNbr(value % other)
+			printInt(value % other)
 		}
 
 	default:
 		return
 	}
+}
+
+func printInt(nbr int) {
+	os.Stdout.WriteString(ConvertNbr(nbr))
+}
+
+func ConvertNbr(nbr int) string {
+	str := ""
+	isNegative := nbr < 0
+
+	for i, remainder := nbr, 0; i != 0; i /= 10 {
+		remainder = i % 10
+		if isNegative {
+			remainder = -remainder
+		}
+		char := rune(remainder) + rune('0')
+		str += string(char)
+	}
+
+	if isNegative {
+		str += "-"
+	} else if nbr == 0 {
+		str = "0"
+	}
+
+	return Reverse(str)
+}
+
+func Reverse(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
 }
