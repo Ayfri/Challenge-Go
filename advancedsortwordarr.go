d6f2e5f0e81ed11bf215d38ae3b8ad069e026d95
Ayfri <pierre.ayfri@gmail.com>
2021-09-19 16:48:35 +0200

    fix: Fix condition, rename argument.

--- advancedsortwordarr.go
+++ advancedsortwordarr.go
@@ -1,10 +1,10 @@
 package piscine
 
-func AdvancedSortWordArr(a []string, f func(a, b string) int) {
+func AdvancedSortWordArr(a []string, compare func(a, b string) int) {
 	length := len(a)
 	for i := 0; i < length; i++ {
 		for j := 0; j < length-i-1; j++ {
-			if f(a[j], a[j+1]) > 0 {
+			if compare(a[j], a[j+1]) != 0 {
 				a[j], a[j+1] = a[j+1], a[j]
 			}
 		}
