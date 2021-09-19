9b97e3bd3cfbf340e0d2394240db744a99fb1899
Ayfri <pierre.ayfri@gmail.com>
2021-09-19 16:17:09 +0200

    fix: Use os.Args.

--- doop/main.go
+++ doop/main.go
@@ -4,7 +4,9 @@ import (
 	"os"
 )
 
-func doop(value int, operator string, other int) {
+func main() {
+	value, operator, other := TrimAtoi(os.Args[0]), os.Args[1], TrimAtoi(os.Args[2])
+
 	switch operator {
 	case "+":
 		printInt(value + other)
@@ -66,4 +68,47 @@ func Reverse(s string) string {
 		runes[i], runes[j] = runes[j], runes[i]
 	}
 	return string(runes)
+}
+
+func TrimAtoi(s string) int {
+	result := 0
+	str := ""
+	isNegative := false
+	for index, char := range s {
+		if index > 0 && s[index-1] == '-' && len(str) == 0 {
+			isNegative = true
+		}
+
+		if isNumber(char) {
+			str += string(char)
+		}
+	}
+
+	str = Reverse(str)
+
+	for index, char := range str {
+		nb := int(char - '0')
+		result += IterativePower(10, index) * nb
+	}
+
+	if isNegative {
+		result = -result
+	}
+
+	return result
+}
+
+func isNumber(char rune) bool {
+	return char >= '0' && char <= '9'
+}
+
+func IterativePower(nb int, power int) int {
+	res := 1
+	if power < 0 {
+		return 0
+	}
+	for i := 1; i <= power; i++ {
+		res = nb * res
+	}
+	return res
 }
