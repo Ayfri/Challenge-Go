22ea458de71086cd12d51f52054c154ac594337a
Ayfri <pierre.ayfri@gmail.com>
2021-09-20 14:42:38 +0200

    refactor: Simplify.

--- basicatoi.go
+++ basicatoi.go
@@ -2,20 +2,12 @@ package piscine
 
 func BasicAtoi(s string) int {
 	var result int
-	for index, char := range s {
+	for _, char := range s {
 		number := int(char - '0')
 		if number < 0 || number > 9 {
 			return 0
 		}
-		result += Pow10(index) * number
+		result *= 10 + number
 	}
 	return result
-}
-
-func Pow10(nb int) int {
-	res := 1
-	for i := 1; i <= nb; i++ {
-		res *= 10
-	}
-	return res
 }
