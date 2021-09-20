2c11aa4c3d768f65b810d0062d849330567a82ee
Ayfri <pierre.ayfri@gmail.com>
2021-09-20 14:50:57 +0200

    fix: Un-simplify.

--- basicatoi2.go
+++ basicatoi2.go
@@ -8,9 +8,8 @@ func BasicAtoi2(s string) int {
 		y++
 	}
 	for x := 0; x <= y-1; x++ {
-		char := int(arr[x] - '0')
-		if char >= 0 && char < 10 {
-			final *= 10 + char
+		if int(arr[x]-'0') >= 0 && int(arr[x]-'0') < 10 {
+			final = final*10 + int(arr[x]-'0')
 		} else {
 			return 0
 		}
