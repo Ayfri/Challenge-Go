11e1d82357e6efa8d25ec6fa44dbfd5875951493
Ayfri <pierre.ayfri@gmail.com>
2021-09-20 14:16:29 +0200

    fix: Revert deleting isLetter.

--- isalpha.go
+++ isalpha.go
@@ -15,3 +15,7 @@ func IsAlpha(s string) bool {
 func isNumber(char rune) bool {
 	return char >= '0' && char <= '9'
 }
+
+func isLetter(char rune) bool {
+	return char >= 'a' && char <= 'z' || char >= 'A' && char <= 'Z'
+}
