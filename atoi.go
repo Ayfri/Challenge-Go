a3fc3bf8c9d788284be3b062693b65532e21144a
Ayfri <pierre.ayfri@gmail.com>
2021-09-20 14:58:08 +0200

    feat: Add quest 6 challenge 11.

---
+++ atoi.go
@@ -0,0 +1,24 @@
+package piscine
+
+func Atoi(s string) int {
+	final := 0
+	isNegative := false
+	start := 0
+	if s[0] == '-' {
+		isNegative = true
+		start++
+	}
+
+	for x := start; x <= len(s)-1; x++ {
+		number := int(s[x] - '0')
+		if number >= 0 && number <= 9 {
+			final = final*10 + number
+		} else {
+			return 0
+		}
+	}
+	if isNegative {
+		return -final
+	}
+	return final
+}
