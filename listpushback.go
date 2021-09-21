7dd231632f7aae1a9e99a4d976c7fe0120e1d317
Ayfri <pierre.ayfri@gmail.com>
2021-09-21 09:09:16 +0200

    feat: Add quest 7 challenge 1.

---
+++ listpushback.go
@@ -0,0 +1,18 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListPushBack(l *List, data interface{}) {
+	*l.Tail, *l.Head = *l.Head, NodeL{
+		data,
+		nil,
+	}
+}
