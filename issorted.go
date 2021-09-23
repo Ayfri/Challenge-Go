90ad481692d66eb86d4aaa18c6d54343943be294
Ayfri <pierre.ayfri@gmail.com>
2021-09-23 15:24:07 +0200

    refactor: Simplify issorted.

--- issorted.go
+++ issorted.go
@@ -1,23 +1,19 @@
 package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
-	isSorted := true
 	if len(a) < 2 {
 		return true
 	}
 
 	isReversed := f(a[0], a[1]) > 0
-	for index, elem := range a {
-		if index+1 < len(a) {
-			elemAfter := a[index+1]
-			if f(elem, elemAfter) <= 0 {
-				if isReversed {
-					isSorted = false
-				}
-			} else if !isReversed {
-				isSorted = false
+	for i := 0; i < len(a)-1; i++ {
+		if f(a[i], a[i]) <= 0 {
+			if isReversed {
+				return false
 			}
+		} else if !isReversed {
+			return false
 		}
 	}
-	return isSorted
+	return true
 }
