66f78b87a4b111a115724b756a76a4ee8a3dc435
Ayfri <pierre.ayfri@gmail.com>
2021-09-28 17:00:25 +0200

    feat: Add quest 10 challenge 1.

---
+++ rot14.go
@@ -0,0 +1,27 @@
+package piscine
+
+func Rot14(s string) string {
+	var result string
+	for _, char := range s {
+		if IsUpper(string(char)) {
+			if char+14 > 'Z' {
+				difference := 'Z' - char
+				char = 'A' + 14 - difference
+				result += string(char)
+			} else {
+				result += string(char + 14)
+			}
+		} else if IsLower(string(char)) {
+			if char+14 > 'z' {
+				difference := 'z' - char
+				char = 'a' + 14 - difference
+				result += string(char)
+			} else {
+				result += string(char + 14)
+			}
+		} else {
+			result += string(char)
+		}
+	}
+	return result
+}
