5c0cf94fe708d5af51f8f0fb5926debb6c243e5c
Ayfri <pierre.ayfri@gmail.com>
2021-09-28 15:12:20 +0200

    feat: Add quest 8 challenge 4.

---
+++ sortparams/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+	"os"
+)
+
+func main() {
+	args := os.Args[1:]
+	
+	SortWordArr(args)
+
+	for _, arg := range args {
+		for _, char := range arg {
+			z01.PrintRune(char)
+		}
+		z01.PrintRune('\n')
+	}
+}
+
+func SortWordArr(a []string) {
+	length := len(a)
+	for i := 0; i < length; i++ {
+		for j := 0; j < length-i-1; j++ {
+			if a[j] > a[j+1] {
+				a[j], a[j+1] = a[j+1], a[j]
+			}
+		}
+	}
+}
