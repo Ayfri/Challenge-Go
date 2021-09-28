90a6590be57eb1a3d6dffa139fe9308ae3c1cc1d
Ayfri <pierre.ayfri@gmail.com>
2021-09-28 15:26:50 +0200

    fix: Don't use fmt.

--- point/main.go
+++ point/main.go
@@ -1,6 +1,9 @@
 package point
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/01-edu/z01"
+)
 
 type point struct {
 	x, y int
@@ -17,4 +20,28 @@ func main() {
 	setPoint(points)
 
 	fmt.Printf("x = %d, y = %d\n", points.x, points.y)
+	result := "x = " + ConvertNbr(points.x) + "y = " + ConvertNbr(points.y) + "\n"
+	for _, char := range result {
+		z01.PrintRune(char)
+	}
+}
+
+func ConvertNbr(nbr int) string {
+	str := ""
+	isNegative := nbr < 0
+
+	for i, remainder := nbr, 0; i != 0; i /= 10 {
+		remainder = i % 10
+		if isNegative {
+			remainder = -remainder
+		}
+		char := rune(remainder) + rune('0')
+		str += string(char)
+	}
+
+	if nbr == 0 {
+		str = "0"
+	}
+
+	return str
 }
