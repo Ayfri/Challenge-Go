3ea681ab1a82adeba26e231972358686fcd5c8f8
Ayfri <pierre.ayfri@gmail.com>
2021-09-28 14:25:22 +0200

    fix: Remove unused function.

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -20,16 +20,3 @@ func main() {
 		z01.PrintRune(char)
 	}
 }
-
-func PrintStr(s string) {
-	lastSlashIndex := 0
-	for index, char := range s {
-		if char == '/' {
-			lastSlashIndex = index
-		}
-	}
-	for i := lastSlashIndex; i < len(s); i++ {
-		char := rune(s[i])
-		z01.PrintRune(char)
-	}
-}
