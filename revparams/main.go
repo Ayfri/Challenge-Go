bcc8c43d05e9f5edeb6ad6b2f521efd7ccd4248a
Ayfri <pierre.ayfri@gmail.com>
2021-09-28 15:09:04 +0200

    feat: Add quest 8 challenge 3.

---
+++ revparams/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+	"os"
+)
+
+func main() {
+	args := os.Args[1:]
+
+	for i, j := 0, len(args)-1; i < j; i, j = i+1, j-1 {
+		args[i], args[j] = args[j], args[i]
+	}
+
+	for _, arg := range args {
+		for _, char := range arg {
+			z01.PrintRune(char)
+		}
+		z01.PrintRune('\n')
+	}
+}
