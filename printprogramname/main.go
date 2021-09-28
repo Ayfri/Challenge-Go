7fecd6c4a380999f4abd51749dc5cd8172789d5d
Ayfri <pierre.ayfri@gmail.com>
2021-09-28 14:19:22 +0200

    feat: Add quest 8 challenge 1.

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -11,7 +11,14 @@ func main() {
 }
 
 func PrintStr(s string) {
-	for _, char := range s {
+	lastSlashIndex := 0
+	for index, char := range s {
+		if char == '/' {
+			lastSlashIndex = index
+		}
+	}
+	for i := lastSlashIndex; i < len(s); i++ {
+		char := rune(s[i])
 		z01.PrintRune(char)
 	}
 }
