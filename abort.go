fc27922492c1659743fb0c1827d71f32acb16249
Ayfri <pierre.ayfri@gmail.com>
2021-09-29 08:57:16 +0200

    fix: Get median with array.

--- abort.go
+++ abort.go
@@ -1,5 +1,13 @@
 package piscine
 
 func Abort(a, b, c, d, e int) int {
-	return (a + b + c + d + e) / 5
+	arr := []int{a, b, c, d, e}
+
+	medianNumber := len(arr) / 2
+
+	if len(arr)%2 == 0 {
+		return arr[medianNumber]
+	}
+
+	return (arr[medianNumber-1] + arr[medianNumber]) / 2
 }
