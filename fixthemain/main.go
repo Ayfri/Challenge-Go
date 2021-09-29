70a3600144665046f7072e1381f55618c354a5ae
Ayfri <pierre.ayfri@gmail.com>
2021-09-29 09:55:32 +0200

    feat: Add quest 10 challenge 7.

---
+++ fixthemain/main.go
@@ -0,0 +1,54 @@
+package main
+
+import "github.com/01-edu/z01"
+
+const (
+	CLOSE = iota
+	OPEN
+)
+
+type Door struct {
+	state int
+}
+
+func PrintStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+}
+
+func CloseDoor(ptrDoor *Door) {
+	PrintStr("Door Closing...")
+	ptrDoor.state = CLOSE
+}
+
+func OpenDoor(ptrDoor *Door) {
+	PrintStr("Door Opening...")
+	ptrDoor.state = OPEN
+}
+
+func IsDoorOpen(Door Door) bool {
+	PrintStr("is the Door opened ?")
+	return Door.state == OPEN
+}
+
+func IsDoorClose(ptrDoor Door) bool {
+	PrintStr("is the Door closed ?")
+
+	return ptrDoor.state == OPEN
+}
+
+func main() {
+	door := &Door{}
+
+	OpenDoor(door)
+	if IsDoorClose(*door) {
+		OpenDoor(door)
+	}
+	if IsDoorOpen(*door) {
+		CloseDoor(door)
+	}
+	if door.state == OPEN {
+		CloseDoor(door)
+	}
+}
