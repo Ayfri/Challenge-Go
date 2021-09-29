c2d82b87cf249d9aa9ac1bedf4332eba06205d65
Ayfri <pierre.ayfri@gmail.com>
2021-09-29 10:14:03 +0200

    refactor: Use variable for empty.

--- compact.go
+++ compact.go
@@ -2,8 +2,9 @@ package piscine
 
 func Compact(ptr *[]string) int {
 	var result []string
+	var empty string
 	for _, element := range *ptr {
-		if element != "" {
+		if element != empty {
 			result = append(result, element)
 		}
 	}
