12aff75ca983653d4ab2383147ae3394ac2793c5
Ayfri <pierre.ayfri@gmail.com>
2021-09-29 09:47:32 +0200

    refactor: Use temporary variables.

--- enigma.go
+++ enigma.go
@@ -1,6 +1,13 @@
 package piscine
 
 func Enigma(a ***int, b *int, c *******int, d ****int) {
-	first, second, third, fourth := ***a, *******c, ****d, *b
-	*******c, ****d, *b, ***a = first, second, third, fourth
+	first := ***a
+	second := *******c
+	third := ****d
+	fourth := *b
+
+	*******c = first
+	****d = second
+	*b = third
+	***a = fourth
 }
