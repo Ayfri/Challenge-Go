4c1bbf350beba106d799337b483a1a631238f1d4
Ayfri <pierre.ayfri@gmail.com>
2021-09-29 09:09:32 +0200

    feat: Add quest 10 challenge 3.

---
+++ collatzcountdown.go
@@ -0,0 +1,19 @@
+package piscine
+
+// divide by two when even
+// multiply by three when odd & add one
+
+func CollatzCountdown(start int) int {
+	for i := 0; i < start; i++ {
+		if start == 1 {
+			return i
+		}
+
+		if start%2 == 0 {
+			start /= 2
+		} else {
+			start = (start * 3) + 1
+		}
+	}
+	return -1
+}
