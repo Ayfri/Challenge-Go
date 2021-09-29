c989bded8742e307d50144959878b8011f661a56
Ayfri <pierre.ayfri@gmail.com>
2021-09-29 08:58:54 +0200

    fix: Sort numbers.

--- abort.go
+++ abort.go
@@ -2,6 +2,7 @@ package piscine
 
 func Abort(a, b, c, d, e int) int {
 	arr := []int{a, b, c, d, e}
+	SortNumbers(arr)
 
 	medianNumber := len(arr) / 2
 
@@ -10,4 +11,15 @@ func Abort(a, b, c, d, e int) int {
 	}
 
 	return (arr[medianNumber-1] + arr[medianNumber]) / 2
+}
+
+func SortNumbers(a []int) {
+	length := len(a)
+	for i := 0; i < length; i++ {
+		for j := 0; j < length-i-1; j++ {
+			if a[j] > a[j+1] {
+				a[j], a[j+1] = a[j+1], a[j]
+			}
+		}
+	}
 }
