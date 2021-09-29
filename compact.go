7da07f626ce9601dadd01215a0162c32371139f0
Ayfri <pierre.ayfri@gmail.com>
2021-09-29 10:04:27 +0200

    fix: Skip if index out of bounds.

--- compact.go
+++ compact.go
@@ -3,6 +3,10 @@ package piscine
 func Compact(ptr *[]string) int {
 	var counter int
 	for index, element := range *ptr {
+		if index >= len(*ptr) {
+			continue
+		}
+
 		if len(element) == 0 {
 			counter++
 			*ptr = append((*ptr)[:index], (*ptr)[index+1:]...)
